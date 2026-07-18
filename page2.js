b1594fda95df6645967c606706d62d37d32d08c0
samaintanartist <sam7mansi@gmail.com>
2026-07-18 20:17:40 +0300

    Update page2.js

--- page2.js
+++ page2.js
@@ -4,14 +4,14 @@ const messages = [
     "what if it turned out that i was planning to hack u with that link to begin with?",
     "Well i barely know how to do type shi like this",
     "i can't hack u even if i wanted to",
-    "Right?"
+    "Right?",
     ":D",
     "Well u gotta be full of energy clicking that much",
     "one more think one more thing",
     "hmm.. i actually forgot what was i planning to say",
     "maybe something that starts with H.. ends with Y..",
     "I'm not sure",
-    "hold on i may have remembered it, Sure u wanna hear it?"
+    "hold on i may have remembered it, Sure u wanna hear it?",
     "if so, click this pink button :D"
 ];
 
